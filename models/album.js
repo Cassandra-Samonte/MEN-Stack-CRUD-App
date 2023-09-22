e0a53cc3b10aa23cbd7e7e7e9cee1e91a63d21e8
Cas <cssamonte@gmail.com>
2023-09-22 14:32:38 -0700

    added seed data

---
+++ models/album.js
@@ -0,0 +1,16 @@
+// Require the Mongoose package
+const mongoose = require('mongoose');
+
+// Create a schema to define the properties of the pets collection
+const albumSchema = new mongoose.Schema({
+    artist: { type: String, required: true },
+    album: { type: String, required: true },
+    releaseDate: { type: Date, required: true },
+    whereToListen: { type: String, required: true },
+    photo: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+// Export the schema as a Monogoose model. 
+// The Mongoose model will be accessed in `models/index.js`
+module.exports = mongoose.model('Album', albumSchema);
