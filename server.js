6586d2df13b74b0f3563c0d02c268aaa1a404c36
Cas <cssamonte@gmail.com>
2023-09-23 11:20:12 -0700

    installed method override - delete route works

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const express = require('express');
 const livereload = require("livereload");
 const connectLiveReload = require("connect-livereload");
+const methodOverride = require('method-override');
+
 
 /* Require the db connection, models, and seed data
 --------------------------------------------------------------- */
@@ -41,11 +43,12 @@ app.set('views', path.join(__dirname, 'views'));
 --------------------------------------------------------------- */
 app.use(express.static('public'))
 app.use(connectLiveReload());
-
 // Body parser: used for POST/PUT/PATCH routes: 
 // this will take incoming strings from the body that are URL encoded and parse them 
 // into an object that can be accessed in the request parameter as a property called body (req.body).
 app.use(express.urlencoded({ extended: true }));
+// Allows us to interpret POST requests from the browser as another request type: DELETE, PUT, etc.
+app.use(methodOverride('_method'));
 
 
 /* Mount routes
@@ -55,10 +58,12 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+
 // About Route
 app.get('/about', function (req, res) {
     res.render('about')
 });
+
 
 /* Seed Route - When a GET request is sent to `/seed`, the albums collection is seeded */
 app.get('/seed', function (req, res) {
@@ -79,6 +84,12 @@ app.get('/seed', function (req, res) {
 // This tells our app to look at the `controllers/albums.js` file 
 // to handle all routes that begin with `localhost:3000/albums`
 app.use('/albums', albumsCtrl)
+
+
+// The "catch-all" route: Runs for any other URL that doesn't match the above routes
+app.get('*', function (req, res) {
+    res.render('404')
+});
 
 
 /* Tell the app to listen on the specified port
